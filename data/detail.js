dfa53e3a3e69e834eb3cbd63ce870d6beb7e0ed4
tlaster <tlaster@outlook.com>
2019-11-22 00:12:05 +0800

    Fix

--- data/detail.js
+++ data/detail.js
@@ -1,14 +1,14 @@
-(async() => {
+(async () => {
     const pLimit = require('p-limit')
     const cliProgress = require('cli-progress')
-    const limit = pLimit(10);
+    const limit = pLimit(100);
     const axios = require("axios").default
     const axiosRetry = require('axios-retry')
     const fs = require("fs")
     const jsonPath = require("jsonpath")
     const cookie = fs.readFileSync("cookie", 'utf8').trim()
-    
-    axiosRetry(axios, { retries: 1000, retryDelay: () => 0 })
+
+    axiosRetry(axios, { retries: 10, retryDelay: () => 0 })
     const data = JSON.parse(fs.readFileSync("./data.json"))
     const wids = jsonPath.query(data, "$..wid")
 
@@ -17,32 +17,23 @@
 
     bar.start(wids.length, 0);
 
-    const tasks = wids.map(id => {
-        limit(async () => {
-            try {
-                const result = await axios.get(`https://webcatalog-free.circle.ms/Circle/${id}/DetailJson`, {
-                    headers: {
-                        cookie
-                    }
-                })
-                
-                bar.increment();
-                return result.data
-            } catch (error) {
-                console.log(`https://webcatalog-free.circle.ms/Circle/${id}/DetailJson`)
-                return {}
-            }
-        })
-    })
-    
-    // const list = []
-    // for await (const task of tasks) {
-    //     list.push(await task)
-    //     await delay(1000)        
-    // }
+    const tasks = wids.map(id => limit(async () => {
+        try {
+            const result = await axios.get(`https://webcatalog-free.circle.ms/Circle/${id}/DetailJson`, {
+                headers: {
+                    cookie
+                }
+            })
+
+            bar.increment();
+            return result.data
+        } catch (error) {
+            console.log(`https://webcatalog-free.circle.ms/Circle/${id}/DetailJson`)
+            return {}
+        }
+    }))
 
     const list = await Promise.all(tasks)
-    
     fs.writeFileSync("./detail.json", JSON.stringify(list));
 
     bar.stop();
