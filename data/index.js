849a2065885121dc3277af6a2bc8a4431ba69ba3
tlaster <tlaster@outlook.com>
2019-11-21 23:52:17 +0800

    Add progress

--- data/index.js
+++ data/index.js
@@ -2,10 +2,12 @@
 (async () => {
     const axios = require("axios").default
     const fs = require("fs")
+    const cliProgress = require('cli-progress');
+    const bar = new cliProgress.SingleBar({}, cliProgress.Presets.rect);
     const cookie = fs.readFileSync("cookie", 'utf8')
-    
     let days = [...Array(4).keys()].map(it => `Day${it + 1}`)
     let halls = ["w12", "w34", "s12", "s34"]
+    bar.start(days.length * halls.length, 0);
     let obj = {}
     for await (const day of days) {
         let dobj = {}
@@ -16,10 +18,11 @@
                 }
             })
             dobj[hall] = result.data
+            bar.increment();
         }
         obj[day] = dobj
     }
     
-    fs.writeFileSync("./data.json", JSON.stringify(obj));
+    fs.writeFileSync("./data2.json", JSON.stringify(obj));
     
 })()
